e34645d7e5e166ee982841b3b73fe3d960f44708
Daniel Khoa <54354759+KhoaNet@users.noreply.github.com>
2021-06-25 10:26:08 +0700

    Update app.js

--- app.js
+++ app.js
@@ -72,4 +72,21 @@ app.use('/bookRental', require('./routes/bookRental'));
 app.use('/publiser', require('./routes/publiser'));
 app.use('/feedbacks', require('./routes/feedbacks'));
 
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
 app.listen(port, () => console.log(`Example app listening on port :${port}!`))
